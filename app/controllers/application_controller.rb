c24eb1cfe04c77fccda0eb54e575a2a63d62b16c
yuporon <yu.murakami@super-studio.jp>
2021-08-17 11:54:35 +0900

    [Add] サインアップ後の遷移先指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    recruits_path
+  end
+
   def after_sign_in_path_for(resource)
     recruits_path
   end
