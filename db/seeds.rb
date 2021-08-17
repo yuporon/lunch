47a265e7b856e7541d81ed69e3cea9a914d76d4a
yuporon <yu.murakami@super-studio.jp>
2021-08-17 14:19:37 +0900

    [Add] seedで登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+  user = User.find_or_initialize_by(email: "user+#{i}@gmail.com")
+  user.update!(
+    name: "#{i}#{i}",
+    password: "#{i}#{i}#{i}#{i}#{i}#{i}",
+    confirmed_at: DateTime.now,
+  )
+end
+
+[
+  { shop: "Eggs 'n Things", location: "銀座", content: "映画にもなった有名なパンケーキ屋さん！", owner_id: 1, status: 0, end_on: DateTime.now + 10 },
+  { shop: "スシロー", location: "どこでも", content: "うまい！やすい！", owner_id: 2, status: 0, end_on: DateTime.now + 11 },
+  { shop: "とみ田", location: "松戸", content: "日本一のつけ麺", owner_id: 3, status: 0, end_on: DateTime.now + 12 },
+  { shop: "叙々苑", location: "どこでも", content: "奢ってください。", owner_id: 1, status: 0, end_on: DateTime.now + 13 },
+  { shop: "たいめいけん", location: "日本橋", content: "店長さんを生で見てみたい", owner_id: 1, status: 0, end_on: DateTime.now + 14 },
+  { shop: "ひみつ堂", location: "台東区", content: "夏はかき氷", owner_id: 2, status: 0, end_on: DateTime.now + 14 },
+].each do |recruit|
+  Recruit.find_or_create_by!(recruit)
+end
