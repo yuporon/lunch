2f47f53689d1cd4e53be198203878b4208b42aba
yuporon <yu.murakami@super-studio.jp>
2021-08-24 13:18:34 +0900

    [Fix] 部分テンプレート化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,20 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_recruit
 
   def create
-    favorite = current_user.favorites.create(recruit_id: params[:id])
+    @favorite = current_user.favorites.create(recruit_id: params[:id])
     redirect_to request.referer
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(recruit_id: params[:id]).destroy
+    @favorite = current_user.favorites.find_by(recruit_id: params[:id]).destroy
     redirect_to request.referer
+  end
+
+  private
+
+  def set_recruit
+    @recruit = Recruit.find(params[:id])
   end
 end
